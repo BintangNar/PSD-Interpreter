d38bda98ec278d5689c0cbde7001dd78c3a28411
BintangNar <bintangnarindra08@gmail.com>
2024-10-27 14:39:18 +0700

    yet another commit

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,25 +1,22 @@
-
 # CONSTANTS
 DIGITS = '0123456789'
 
-#ERROR
+# ERROR
 class errors:
-    def __init__(self,error_name,details):
+    def __init__(self, error_name, details):
         self.error_name = error_name
         self.details = details
     
     def as_string(self):
-        result = f'{self.error_name}: {self.details}'
-        return result
+        return f'{self.error_name}: {self.details}'
 
 class illegalChar(errors):
-    def __init__(self, error_name, details):
+    def __init__(self, details):
         super().__init__('Illegal Character Detected', details)
 
-# MEMBUAT TOKEN
-
+# TOKENS
 TT_INT = 'TT_INT'
-TT_FLOAT = 'FLOAT'
+TT_FLOAT = 'TT_FLOAT'
 TT_PLUS = 'PLUS'
 TT_MINUS = 'MINUS'
 TT_MUL = 'MUL'
@@ -27,17 +24,17 @@ TT_DIV = 'DIV'
 TT_MOD = 'MOD'
 
 class token:
-    def __init__(self, type_,value=None):
+    def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
     def __repr__(self):
-        if self.value: return f'{self.tpe}:{self.value}'
+        if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
 
 # LEXER
 class lexer:
-    def __init__(self,text):
+    def __init__(self, text):
         self.text = text
         self.pos = -1
         self.current_char = None
@@ -48,57 +45,58 @@ class lexer:
         self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
     
     def tokenCreate(self):
-        token = []
+        tokens = []
 
-        while self.current_char != None:
+        while self.current_char is not None:
             if self.current_char in ' \t':
                 self.advance()
             elif self.current_char == '+':
-                token.append(Token(TT_PLUS))
+                tokens.append(token(TT_PLUS))
                 self.advance()
             elif self.current_char == '-':
-                token.append(Token(TT_MINUS))
+                tokens.append(token(TT_MINUS))
                 self.advance()
             elif self.current_char == '*':
-                token.append(Token(TT_MUL))
+                tokens.append(token(TT_MUL))
                 self.advance()
             elif self.current_char == '/':
-                token.append(Token(TT_DIV))
+                tokens.append(token(TT_DIV))
                 self.advance()
             elif self.current_char == '%':
-                token.append(Token(TT_MOD))
+                tokens.append(token(TT_MOD))
                 self.advance()
             elif self.current_char in DIGITS:
-                token.append(self.make_number())
+                tokens.append(self.make_number())
             else:
                 char = self.current_char
-                self.advance
-                return[], illegalChar("'" + char + "' is not listed")
+                self.advance()
+                return [], illegalChar("'" + char + "' is not listed")
             
-        return token,None
+        return tokens, None
     
     def make_number(self):
         num_str = ''
         dot_count = 0
 
-        while self.current_char != None and self.current_char in DIGITS + '.':
+        while self.current_char is not None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
-                if dot_count == 1: break
+                if dot_count == 1: 
+                    break
                 dot_count += 1
                 num_str += '.'
             else:
                 num_str += self.current_char
             self.advance()
-            
-            if dot_count == 0:
-                return token(TT_INT, int(num_str))
-            else:
-                return token(TT_FLOAT,float(num_str))
+        
+        
+        if dot_count == 0:
+            return token(TT_INT, int(num_str))
+        else:
+            return token(TT_FLOAT, float(num_str))
             
 # Program Run
-
 def run(text):
-    lexer = lexer(text)
-    token,errors = lexer.tokenCreate()
+    lex = lexer(text)  
+    tokens, error = lex.tokenCreate()
 
-    return token,errors
+    return tokens, error
